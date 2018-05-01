66cb6c6d751b25963764db2681a13d39ec9caa60
dr10n9 <dr10n9@gmail.com>
2018-05-01 13:38:12 +0000

    scripts added

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, send_file
+import requests
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return(open("/app/index.html", "r").read())
+
+
+@app.route('/ip')
+def ip():
+    r = requests.get(r'http://jsonip.com')
+    ip = r.json()['ip']
+    return(ip)
+
+
+@app.route('/css/style.css')
+def style():
+    return(open('/app/css/style.css', 'r').read())
+
+
+@app.route('/images')
+def image():
+    return send_file('/app/images/'+request.args.get('name'),
+                     mimetype='image/gif')
