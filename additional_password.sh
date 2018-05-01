a4050be49770933e96cbc0784c5977cbcd2703f5
dr10n9 <dr10n9@gmail.com>
2018-05-01 17:59:59 +0000

    shellcheck done

--- additional_password.sh
+++ additional_password.sh
@@ -1,22 +1,22 @@
 
 #this code finds 3 parts of password  
-quantity=$(cat /app/codes | wc -l)
+quantity=$(cmd < /app/codes | wc -l)
 for((i=1;i<=quantity;i++))
 do
 	arr[i]=$(head -$i codes | tail -1)
-	arr2[i]=$(cat /var/log/nginx/old.log | grep ${arr[i]} | wc -l)
+	arr2[i]=$(cmd < /var/log/nginx/old.log | grep -c ${arr[i]})
 done
 
 max=${arr[1]}
 for((i=1;i<=quantity;i++))
 do
-	if((arr2[i]>$max)); then
+	if((arr2[i]>max)); then
 		max=${arr[i]}
         fi
 done
 part1=$max
 
-part2=$(cat /var/log/nginx/old.log | grep '=8.8.8.8' | wc -l)
-part3=$(curl -I hint.macpaw.io | grep 'ETag');part3=$(echo ${part3:7:2})
-pass=$(($part1+$part2+$part3))
+part2=$(cmd < /var/log/nginx/old.log | grep -c '=8.8.8.8')
+part3=$(curl -I hint.macpaw.io | grep 'ETag');part3=$(cmd < ${part3:7:2})
+pass=$((part1+part2+part3))
 echo $pass > /app/pass
--- change_configs.sh
+++ change_configs.sh
@@ -1,6 +1,7 @@
 
-#updateing nginx and supervisord conig files
-awk '/--ini/{gsub(/--ini/, "--callable app --ini")};{print}' /etc/supervisor/conf.d/supervisord.conf > tmp;
+#updating nginx and supervisord conig files
+awk '/--ini/{gsub(/--ini/, "--callable app --ini")};{print}'\
+/etc/supervisor/conf.d/supervisord.conf > tmp;
 cat tmp > /etc/supervisor/conf.d/supervisord.conf; rm -f tmp;
 cp nginx.conf /etc/nginx/conf.d/nginx.conf;
-cd /app; pip install -r requirements.txt;
+cd /app || exit; pip install -r requirements.txt;
