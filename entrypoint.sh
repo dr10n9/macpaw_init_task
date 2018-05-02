dc4f1edfb77a9c00889cc22f09d0cd7f40d227af
dr10n9 <dr10n9@gmail.com>
2018-05-02 12:39:26 +0000

    edit entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ if ! grep -q "listen ${USE_LISTEN_PORT};" /etc/nginx/conf.d/nginx.conf ; then
     sed -i -e "/server {/a\    listen ${USE_LISTEN_PORT};" /etc/nginx/conf.d/nginx.conf
 fi
 
-#this code finds 3 parts of password  
+#find 3 parts of passsword to unzip additional.zip
 quantity=$(cat /app/codes | wc -l)
 for((i=1;i<=quantity;i++))
 do
@@ -40,9 +40,13 @@ part3=$(curl -I hint.macpaw.io | grep 'ETag');part3=$(echo ${part3:7:2})
 pass=$(($part1+$part2+$part3))
 echo $pass > /app/pass
 
-#updating nginx and supervisord conig files
+#update nginx and supervisord conig files
+#callable to run flask app
 awk '/--ini/{gsub(/--ini/, "--callable app --ini")};{print}' /etc/supervisor/conf.d/supervisord.conf > tmp;
-cat tmp > /etc/supervisor/conf.d/supervisord.conf; rm -f tmp;
+cat tmp > /etc/supervisor/conf.d/supervisord.conf;
+rm -f tmp;
 cp nginx.conf /etc/nginx/conf.d/nginx.conf;
-cd /app; pip install -r requirements.txt;
+cd /app;
+#install packages from requirements.txt
+pip install -r requirements.txt;
 exec "$@"
