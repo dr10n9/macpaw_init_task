042f8b215ab4710e8965dc0a98cfd4911cdd278b
root <dr10n9@gmail.com>
2018-05-04 11:30:40 +0000

    create test.html to check new design

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def index():
     return(open("/app/index.html", "r").read())
 
 
+@app.route('/test')
+def test():
+    return(open("/app/test.html", "r").read())
+
+
 @app.route('/ip')
 def ip():
     r = requests.get(r'http://jsonip.com')
